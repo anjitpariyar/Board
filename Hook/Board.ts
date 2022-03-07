efef7214a107ee44cd55eccbadf9ca7b7298b462
anjitpariyar <anjitpariyar@gmail.com>
2022-03-07 23:12:36 +0545

    paginationStyle added

--- Hook/Board.ts
+++ Hook/Board.ts
@@ -1,5 +1,5 @@
 import { useMemo } from "react";
-
+import styled from "styled-components";
 /**
  *
  * @param totalCount required, pageSize is total number of row in one page default is 10, currentPage default 0
@@ -25,4 +25,40 @@ export const paginationController = ({ page }) => {
     const newPage = page;
     return { newPage };
   }, [page]);
+};
+
+export const paginationStyle = ({
+  color = "#000",
+  backgroundColor = "transparent",
+  activeBackground = "rgba(0, 0, 0, 0.04)",
+  activeColor = "#000",
+}) => {
+  const Ul = styled.ul`
+    display: flex;
+    list-style-type: none;
+  `;
+
+  const Li = styled.li<{ active?: boolean }>`
+    padding: 0 12px;
+    height: 32px;
+    text-align: center;
+    margin: auto 4px;
+    color: ${({ active }) => (active ? activeColor : color)};
+    display: flex;
+    box-sizing: border-box;
+    align-items: center;
+    letter-spacing: 0.01071em;
+    border-radius: 16px;
+    line-height: 1.43;
+    font-size: 13px;
+    min-width: 32px;
+    background-color: ${({ active }) =>
+      active ? activeBackground : backgroundColor};
+    cursor: pointer;
+    &:hover {
+      background-color: ${activeBackground};
+    }
+  `;
+
+  return { Ul, Li };
 };
--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { usePagination } from "Hook/Board";
+import { usePagination, paginationStyle } from "Hook/Board";
 
 interface Props {
   onPageChange?: any;
@@ -19,6 +19,14 @@ const Pagination = ({ onPageChange, style, currentPage, pageSize }: Props) => {
   });
 
   const { totalPageCount } = Instance;
+
+  //options
+  // color = "#000",
+  // backgroundColor = "transparent",
+  // activeBackground = "rgba(0, 0, 0, 0.04)",
+  // activeColor = "#000",
+  const styleInstance = paginationStyle({});
+  const { Ul, Li } = styleInstance;
 
   return (
     <>
@@ -40,28 +48,3 @@ const Pagination = ({ onPageChange, style, currentPage, pageSize }: Props) => {
 };
 
 export default Pagination;
-
-const Ul = styled.ul`
-  display: flex;
-  list-style-type: none;
-`;
-const Li = styled.li<{ active?: boolean }>`
-  padding: 0 12px;
-  height: 32px;
-  text-align: center;
-  margin: auto 4px;
-  color: rgba(0, 0, 0, 0.87);
-  display: flex;
-  box-sizing: border-box;
-  align-items: center;
-  letter-spacing: 0.01071em;
-  border-radius: 16px;
-  line-height: 1.43;
-  font-size: 13px;
-  min-width: 32px;
-  background-color: ${({ active }) => active && "rgba(0, 0, 0, 0.04)"};
-  cursor: pointer;
-  &:hover {
-    background-color: rgba(0, 0, 0, 0.04);
-  }
-`;
